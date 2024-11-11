fdc97dfb46557681e527cabe7d31e29afe309153
kramalmr <akram3107@student.abudzar.sch.id>
2024-11-11 11:33:28 +0700

    tugas3

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './meeting-2/App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./meeting-3/tugas3.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
