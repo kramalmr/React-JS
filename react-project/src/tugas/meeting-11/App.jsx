e751c755e88b8d8970d0cf00b9e87811ae8059ee
gitAlmair <160695360+kramalmr@users.noreply.github.com>
2025-01-13 11:15:24 +0700

    tugas 11

---
+++ react-project/src/tugas/meeting-11/App.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+
+// SOAL 1
+const ToggleSwitch = () => {
+    const theme = useTheme();
+    
+    return (
+        <div className="toggle-switch-container">
+            <label className="toggle-switch">
+                <input type="checkbox" id='toggle'/>
+                <span className="toggle-switch-label"></span>
+            </label>
+            <h1>{theme}</h1>
+        </div>
+    );
+};
+
+const useTheme = () => {
+    const [theme, setTheme] = useState('light');
+
+    const handleChange = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+        console.log("Theme berubah menjadi : " + newTheme);
+    }
+    
+    useEffect(() => {
+        const toggleSwitch = document.getElementById('toggle');
+        
+        toggleSwitch.addEventListener("change", handleChange);
+        
+        return () => {
+            toggleSwitch.removeEventListener("change", handleChange);
+        };
+    }, [theme]);
+
+    return theme;
+}
+
+
+// SOAL 2
+
+const ToggleTask = () => {
+    const toggleTaskStatus = useToggleTask();
+
+    return (
+        <div>
+            <h1>{toggleTaskStatus ? '✅ Task Completed' : '❌ Task Not Completed'}</h1>
+            <button id='buttonToggle'>Toggle Task</button>
+        </div>
+    )
+}
+
+const useToggleTask = () => {
+    const [isTaskCompleted, setIsTaskCompleted] = useState(false);
+    const handleClick = () => {
+        setIsTaskCompleted(!isTaskCompleted);
+        // console.log(isTaskCompleted);
+    }
+
+    useEffect(() => {
+        const buttonToggle = document.getElementById('buttonToggle');
+        buttonToggle.addEventListener('click', handleClick);
+
+        return () => {
+            buttonToggle.removeEventListener('click', handleClick);
+        };
+    }, [isTaskCompleted]);
+
+    return isTaskCompleted;
+}
+
+function App() {
+    return (
+        <div>
+            <h1>SOAL 1</h1>
+            <ToggleSwitch />
+
+            <h1>SOAL 2</h1>
+            <ToggleTask />
+        </div>
+    )
+}
+
+export default App
